94bdcaface8c510edb3bc744a5a92d7a7d79ef62
suraj <sharadadde@gmail.com>
2025-09-06 23:15:20 +0530

    fixed the hooks

--- opal-web-version/src/hooks/useRenameFolders.ts
+++ opal-web-version/src/hooks/useRenameFolders.ts
@@ -2,5 +2,12 @@ import { useRef } from 'react'
 
 export const useRenameFolders = (folderId: string) => {
   const inputRef = useRef<HTMLInputElement | null>(null)
-  const folderCardRef
+  const folderCardRef = useRef<HTMLDivElement | null>(null)
+  
+  // TODO: Add folder rename functionality here
+  
+  return {
+    inputRef,
+    folderCardRef
+  }
 }
