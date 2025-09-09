1ffbbb06ffcf20677cd19faef4d4d2e19a9da872
suraj <sharadadde@gmail.com>
2025-09-09 19:19:13 +0530

    try again-27

--- opal-web-version/src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ opal-web-version/src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import { SignIn } from '@clerk/nextjs'
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
 
 type Props = {}
 
 const SignInPage = async (props: Props) => {
-  // Check if user is already signed in
-  const user = await currentUser()
-  
-  if (user) {
-    // User is already signed in, redirect to callback to handle dashboard redirect
-    redirect('/auth/callback')
-  }
-
+  // Do not server-redirect when a user session exists.
+  // Let Clerk's widget handle post-auth navigation to avoid redirect loops
+  // when /auth/callback encounters an error.
   return <SignIn redirectUrl="/auth/callback" fallbackRedirectUrl="/auth/callback" />
 }
 
