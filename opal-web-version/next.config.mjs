18a073853b50c6cfd4dca0d2e5cf2af7fc261695
suraj <sharadadde@gmail.com>
2025-09-09 18:08:46 +0530

    try again-22

--- opal-web-version/next.config.mjs
+++ opal-web-version/next.config.mjs
@@ -4,29 +4,6 @@ const nextConfig = {
         // Allow Prisma to be used in server components
         serverComponentsExternalPackages: ['@prisma/client'],
     },
-    // Webpack configuration to handle client reference manifests properly
-    webpack: (config, { dev, isServer }) => {
-        // Handle client reference manifest generation for route groups
-        if (!dev && !isServer) {
-            config.optimization.splitChunks = {
-                ...config.optimization.splitChunks,
-                cacheGroups: {
-                    ...config.optimization.splitChunks.cacheGroups,
-                    default: false,
-                    vendors: false,
-                    // Create a chunk for route groups
-                    routeGroups: {
-                        name: 'route-groups',
-                        chunks: 'all',
-                        test: /[\\/]app[\\/]\([^)]+\)[\\/]/,
-                        priority: 10,
-                        reuseExistingChunk: true,
-                    },
-                },
-            }
-        }
-        return config
-    },
     // No redirects for now
     async redirects() {
         return []
